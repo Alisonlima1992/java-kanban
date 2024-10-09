45b19fb11d9e971bbae7299504de1e7dbdab2764
Alisonlima <aurin7128@gmail.com>
2024-10-10 01:13:49 +0700

    initial commit with Idea

---
+++ com/practicum/model/Subtask.java
@@ -0,0 +1,16 @@
+package com.practicum.model;
+import com.practicum.service.Status;
+public class Subtask extends Task{
+    private final int epicId;
+
+    public Subtask(int id, String title, String description, Status status, int epicId) {
+        super(id, title, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+}
+
+
