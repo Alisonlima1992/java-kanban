7dce73dd2fcdbb158dff525af984d9d9088d59e3
Alisonlima <aurin7128@gmail.com>
2024-10-10 00:57:50 +0700

    initial commit with Idea

---
+++ com/practicum/Main.java
@@ -0,0 +1,13 @@
+package com.practicum;
+import com.practicum.service.Status;
+import com.practicum.service.TaskManager;
+import com.practicum.model.Task;
+import com.practicum.model.Epic;
+import com.practicum.model.Subtask;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
