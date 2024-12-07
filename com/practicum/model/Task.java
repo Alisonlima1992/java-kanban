7d2f9e0d93d4ac333f56430eb392d55121e34eeb
Alisonlima <aurin7128@gmail.com>
2024-12-07 14:26:24 +0700

    Changes

---
+++ com/practicum/model/Task.java
@@ -0,0 +1,63 @@
+package com.practicum.model;
+
+import com.practicum.service.Status;
+
+public class Task {
+    private final int id;
+    private final String title;
+    private String description;
+    private Status status;
+
+    public Task(int id, String title, String description, Status status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Task task = (Task) obj;
+
+        return id == task.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
+
